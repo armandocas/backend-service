9b6c6896e542fec57bc2895fcce84822420ed0e4
armandocas <armandoneto18@gmail.com>
2025-01-04 18:05:02 -0300

    chore: change cors

--- app.js
+++ app.js
@@ -5,7 +5,18 @@ import formRoutes from './src/routes/formRoutes.js';
 
 const app = express();
 
-app.use(cors());
+// app.use(cors()); Todas as rotas podem ser acessadas
+
+app.use(cors({
+    origin: [
+        'https://landing-page-738owdtpb-thedjangocowboys-projects.vercel.app/api', // Origem do seu frontend
+        'https://backend-service-wine.vercel.app/api', // Origem do backend (se necessário)
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+      allowedHeaders: ['Content-Type'], // Cabeçalhos permitidos
+  }));
+  
+
 app.use(bodyParser.json());
 app.use('/api', formRoutes);
 
