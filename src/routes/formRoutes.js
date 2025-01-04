427d5b65c3799d9ab9dfe7afde708daec763bda6
armandocas <armandoneto18@gmail.com>
2025-01-04 17:10:09 -0300

    chore: change formroutes file

--- src/routes/formRoutes.js
+++ src/routes/formRoutes.js
@@ -3,6 +3,12 @@ import { submitForm } from '../controllers/formController.js';
 
 const router = express.Router();
 
+// Rota para o caminho raiz
+router.get('/', (req, res) => {
+  res.send('Bem-vindo ao Backend Service! A API está funcionando corretamente.');
+});
+
+// Rota para submissão do formulário
 router.post('/submit', submitForm);
 
 export default router;
